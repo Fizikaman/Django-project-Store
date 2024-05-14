c8c5e4eacfbd189a93469829a1cd5aae653b488c
Fizikaman <fizikaman26@gmail.com>
2024-05-14 23:24:47 +0300

    небольшие изменения в админке

--- store/products/admin.py
+++ store/products/admin.py
@@ -5,3 +5,13 @@ from products import models
 
 admin.site.register(models.Product)
 admin.site.register(models.ProductCategory)
+
+class BasketAdmin(admin.TabularInline):
+    model = models.Basket
+    fields = [
+        'product',
+        'quantity',
+        'created',
+    ]
+    extra = 0
+    readonly_fields = ['created']
