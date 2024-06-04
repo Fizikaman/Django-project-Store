869bd9fd6cd2681e29b3855822de70633cc0faaa
Fizikaman <fizikaman26@gmail.com>
2024-06-04 23:26:34 +0300

    перешел на посгрес

--- store/store/settings.py
+++ store/store/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'store_db',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get("PASSWORD"),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
